bfb9a62788beec7651a2319a35038378e3d1c405
Legata <legata93@gmail.com>
2023-07-29 17:03:58 +0300

    full release 1.0

--- public/homePage.js
+++ public/homePage.js
@@ -1,4 +1,26 @@
 const logoutButton = new LogoutButton()
+const ratesBoard = new RatesBoard()
+const moneyManager = new MoneyManager()
+const favoritesWidget = new FavoritesWidget()
+
+function responseFavorites(response) {
+    if (response.success) {
+        favoritesWidget.clearTable()
+        favoritesWidget.fillTable(response.data)
+        moneyManager.updateUsersList(response.data)
+    } else {
+        favoritesWidget.setMessage(response.success, response.error)
+    }
+}
+
+function responseMoney(response) {
+    if (response.success) {
+        moneyManager.setMessage(response.success, 'Успешно')
+        ProfileWidget.showProfile(response.data)
+    } else {
+        moneyManager.setMessage(response.success, response.error)
+    }
+}
 
 logoutButton.action = () => {
     ApiConnector.logout(response => {
@@ -7,3 +29,38 @@ logoutButton.action = () => {
         }
     })
 }
+
+ApiConnector.current(response => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data)
+    }
+})
+
+function getStocksValute() {
+    ApiConnector.getStocks(response => {
+        if (response.success) {
+            ratesBoard.clearTable()
+            ratesBoard.fillTable(response.data)
+        }
+    })
+}
+getStocksValute()
+setInterval(getStocksValute, 60000)
+
+moneyManager.addMoneyCallback = data => {
+    ApiConnector.addMoney(data, responseMoney) 
+    }
+moneyManager.conversionMoneyCallback = data => {
+     ApiConnector.convertMoney(data, responseMoney) 
+    }
+moneyManager.sendMoneyCallback = data => {
+     ApiConnector.transferMoney(data, responseMoney) 
+    }
+
+ApiConnector.getFavorites(responseFavorites)
+favoritesWidget.addUserCallback = data => {
+     ApiConnector.addUserToFavorites(data, responseFavorites) 
+    }
+favoritesWidget.removeUserCallback = id => {
+     ApiConnector.removeUserFromFavorites(id, responseFavorites) 
+    }
